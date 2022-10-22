aca0b83c446064d878b6cdc5cd22dab9259a0d7e
Sergei Kondratev <92305401+DERKET@users.noreply.github.com>
2022-10-22 21:00:32 +0300

    feat: update app.js file

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
+const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
+const start = document.getElementById("start");
+
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover)
+    placeholder.addEventListener('dragenter', dragenter)
+    placeholder.addEventListener('dragleave', dragleave)
+    placeholder.addEventListener('drop', dragdrop)
+}
+
+function dragstart(event) {
+    event.target.classList.add('hold')
+    setTimeout(() => event.target.classList.add('hide'), 0)
+}
+
+function dragend(event) {
+    event.target.className = 'item'
+}
+
+function dragenter(event) {
+    event.target.classList.add('hovered')
+}
+
+function dragover(event) {
+    event.preventDefault()
+}
+
+
+function dragleave(event) {
+    event.target.classList.remove('hovered')
+}
+
+function dragdrop(event) {
+    event.target.append(item)
+    event.target.classList.remove('hovered')
+}
+
+
 
